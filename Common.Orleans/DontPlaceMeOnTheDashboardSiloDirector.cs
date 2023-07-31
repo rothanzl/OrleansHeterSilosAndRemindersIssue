8bb5e7e24a79fe4e33e15b8cea7957473dcae5ff
michalrothanzl <michal.rothanzl@stratox.cz>
2023-07-31 16:21:57 +0200

    Log in placement strategy

--- Common.Orleans/DontPlaceMeOnTheDashboardSiloDirector.cs
+++ Common.Orleans/DontPlaceMeOnTheDashboardSiloDirector.cs
@@ -1,4 +1,5 @@
-﻿using Orleans.Placement;
+﻿using Microsoft.Extensions.Logging;
+using Orleans.Placement;
 using Orleans.Runtime;
 using Orleans.Runtime.Placement;
 
@@ -8,16 +9,19 @@ namespace Common.Orleans
     {
         public IGrainFactory GrainFactory { get; set; }
         public IManagementGrain ManagementGrain { get; set; }
+        private readonly ILogger<DontPlaceMeOnTheDashboardSiloDirector> _logger;
 
-        public DontPlaceMeOnTheDashboardSiloDirector(IGrainFactory grainFactory)
+        public DontPlaceMeOnTheDashboardSiloDirector(IGrainFactory grainFactory, ILogger<DontPlaceMeOnTheDashboardSiloDirector> logger)
         {
             GrainFactory = grainFactory;
+            _logger = logger;
             ManagementGrain = GrainFactory.GetGrain<IManagementGrain>(0);
         }
 
         public async Task<SiloAddress> OnAddActivation(PlacementStrategy strategy, PlacementTarget target, IPlacementContext context)
         {
             var activeSilos = await ManagementGrain.GetDetailedHosts(onlyActive: true);
+            _logger.LogInformation($"Silos[{activeSilos.Length}] wiht names {string.Join(", ", activeSilos.Select(s => s.RoleName))}");
             var silos = activeSilos.Where(x => !x.RoleName.ToLower().Contains("dashboard")).Select(x => x.SiloAddress).ToArray();
             return silos[new Random().Next(0, silos.Length)];
         }
