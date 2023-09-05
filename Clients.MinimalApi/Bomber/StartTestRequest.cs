5c7259ca78951554ce73ab6926fcc866cda1d430
michalrothanzl <michal.rothanzl@stratox.cz>
2023-09-05 10:25:31 +0200

    Increase resource group

--- Clients.MinimalApi/Bomber/StartTestRequest.cs
+++ Clients.MinimalApi/Bomber/StartTestRequest.cs
@@ -4,7 +4,7 @@ namespace Clients.MinimalApi.Bomber;
 
 public record StartTestRequest(
     [property:DefaultValue(10)] int Concurrency, 
-    [property:DefaultValue(0)] double WarmingUpDurationSeconds,
+    [property:DefaultValue(5)] double WarmingUpDurationSeconds,
     [property:DefaultValue(0)] double RampMinutes, 
     [property:DefaultValue(60)] double DurationMinutes,
     [property:DefaultValue(0)] long CounterStartValue,
