0ff314115d215c39cf19518b487a8ae1f91e1eab
michalrothanzl <michal.rothanzl@stratox.cz>
2023-09-07 09:53:53 +0200

    Disable grain state

--- Silo/TestGrains/RecurrentTestGrainInMemory.cs
+++ Silo/TestGrains/RecurrentTestGrainInMemory.cs
@@ -12,28 +12,28 @@ namespace Silo.TestGrains;
 [PreferLocalPlacement]
 public class RecurrentTestGrainInMemory : Grain, IRecurrentTestGrainInMemory
 {
-    private DemoState State { get; set; } = new();
+    // private DemoState State { get; set; } = new();
     
     public override Task OnActivateAsync(CancellationToken cancellationToken)
     {
-        var hydrator = new Hydrator<DemoState>()
-                .With(x=>x.Address, new Hydrator<DemoState.LockerAddress>())
-                .With(x => x.LastTelemeries, new Hydrator<LockerTelemetryDto>()
-                    .With(x=> x.ShutdownReasons, new Hydrator<LockerShutdownReasonType>().GetList(10).ToImmutableList())
-                    .GetList(10))
-                .With(x=>x.LockerHealthCheck, new Hydrator<LockerHealthCheckOut>())
-                .With(x => x.SigningKeys, new Hydrator<DemoState.LockerSigningKeys>())
-                .With(x => x.Alerts, new Hydrator<DemoState.LockerActiveAlert>().GetList(10))
-                .With(x => x.LockerCommands, new Hydrator<Guid>().GetList(10))
-                .With(x => x.Inventories, new Hydrator<DemoState.LockerInventoryConnector>().GetList(10))
-                .With(x => x.Incidents, new Hydrator<DemoState.LockerActiveIncident>().GetList(10))
-                .With(x => x.MalfunctionSlots, new Hydrator<DemoState.SlotAggregation>()
-                    .With(x => x.SlotIndexes, new Hydrator<byte>().GetList(10)))
-                .With(x => x.InventorySlots, new Hydrator<DemoState.SlotAggregation>()
-                    .With(x => x.SlotIndexes, new Hydrator<byte>().GetList(10)))
-            ;
-        
-        State = hydrator.Generate();
+        // var hydrator = new Hydrator<DemoState>()
+        //         .With(x=>x.Address, new Hydrator<DemoState.LockerAddress>())
+        //         .With(x => x.LastTelemeries, new Hydrator<LockerTelemetryDto>()
+        //             .With(x=> x.ShutdownReasons, new Hydrator<LockerShutdownReasonType>().GetList(10).ToImmutableList())
+        //             .GetList(10))
+        //         .With(x=>x.LockerHealthCheck, new Hydrator<LockerHealthCheckOut>())
+        //         .With(x => x.SigningKeys, new Hydrator<DemoState.LockerSigningKeys>())
+        //         .With(x => x.Alerts, new Hydrator<DemoState.LockerActiveAlert>().GetList(10))
+        //         .With(x => x.LockerCommands, new Hydrator<Guid>().GetList(10))
+        //         .With(x => x.Inventories, new Hydrator<DemoState.LockerInventoryConnector>().GetList(10))
+        //         .With(x => x.Incidents, new Hydrator<DemoState.LockerActiveIncident>().GetList(10))
+        //         .With(x => x.MalfunctionSlots, new Hydrator<DemoState.SlotAggregation>()
+        //             .With(x => x.SlotIndexes, new Hydrator<byte>().GetList(10)))
+        //         .With(x => x.InventorySlots, new Hydrator<DemoState.SlotAggregation>()
+        //             .With(x => x.SlotIndexes, new Hydrator<byte>().GetList(10)))
+        //     ;
+        //
+        // State = hydrator.Generate();
         
         DelayDeactivation(TimeSpan.FromHours(24));
         
