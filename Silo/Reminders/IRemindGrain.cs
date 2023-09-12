6b51dd1d507bf752eed0fcb276666c55253096a2
tomas husnik <tomas.husnik@packeta.com>
2023-09-12 14:32:56 +0200

    IReminder test 1

--- Silo/Reminders/IRemindGrain.cs
+++ Silo/Reminders/IRemindGrain.cs
@@ -1,4 +1,7 @@
+using Newtonsoft.Json;
 using Orleans.Runtime;
+using Silo.TestGrains.States;
+using System.Security.Policy;
 
 namespace Silo.Reminders;
 
@@ -9,13 +12,60 @@ public interface IRemindGrain : IGrainWithStringKey
 
 public class RemindGrain : Grain, IRemindGrain, IRemindable
 {
+    
     public Task ReceiveReminder(string reminderName, TickStatus status)
     {
-        throw new NotImplementedException();
+        return Task.Delay(2000);
     }
 
-    public ValueTask Init()
+    public async ValueTask Init()
     {
-        throw new NotImplementedException();
+        string json = @"{
+            'Id': 123456789,
+            'TenantId': '00000000-0000-0000-0000-000000000000',
+            'SlotId': 123456789,
+            'SlotIndex': 255,
+            'CabinetIndex': 255,
+            'CabinetId': 123456789,
+            'LockerId': 123456789,
+            'DroppedSlotId': 123456789,
+            'State': 'ShipmentStateLegacyValue',
+            'StateName': 'ShipmentStateValue',
+            'Width': 32767,
+            'Height': 32767,
+            'Depth': 32767,
+            'Weight': 2147483647,
+            'Created': '2023-09-12T12:34:56.789Z',
+            'PickUpTo': '2023-09-12T23:59:59.999Z',
+            'Dropped': '2023-09-12T12:34:56.789Z',
+            'Canceled': '2023-09-12T12:34:56.789Z',
+            'Returned': '2023-09-12T12:34:56.789Z',
+            'PickedUp': '2023-09-12T12:34:56.789Z',
+            'PickUpCode': 'PickUpCodeValue',
+            'DropOffCode': 'DropOffCodeValue',
+            'DropOffRole': 'OperationRoleValue',
+            'DropOffByActor': 'DropOffByActorValue',
+            'DropOffByDeviceId': 'DropOffByDeviceIdValue',
+            'DropOffCommitedByActor': 'DropOffCommitedByActorValue',
+            'DropOffCommited': '2023-09-12T12:34:56.789Z',
+            'DropOffCommitedByDeviceId': 'DropOffCommitedByDeviceIdValue',
+            'DropOffCommitedByRole': 'OperationRoleValue',
+            'PickUpRole': 'OperationRoleValue',
+            'PickupCommited': '2023-09-12T12:34:56.789Z',
+            'PickUpByActor': 'PickUpByActorValue',
+            'PickUpByDeviceId': 'PickUpByDeviceIdValue',
+            'PickUpCommitedByActor': 'PickUpCommitedByActorValue',
+            'PickUpCommitedByDeviceId': 'PickUpCommitedByDeviceIdValue',
+            'PickUpCommitedByRole': 'OperationRoleValue',
+            'SenderExternalId': 'SenderExternalIdValue',
+            'RecipientExternalId': 'RecipientExternalIdValue',
+            'NumberOfPackages': 123456789,
+            'ExternalId': 'ExternalIdValue',
+            'DestinationLockerId': 123456789,
+            'DestinationLockerExternalId': 'DestinationLockerExternalIdValue'
+        }";
+
+        ShipmentDemoState shipment = JsonConvert.DeserializeObject<ShipmentDemoState>(json);
+       await this.RegisterOrUpdateReminder("shipmentState", TimeSpan.FromMinutes(5), TimeSpan.FromMinutes(5));
     }
 }
