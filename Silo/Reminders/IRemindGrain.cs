77a853fa781eca66b432ceb17da06852d6111629
michalrothanzl <michal.rothanzl@stratox.cz>
2023-09-12 14:04:30 +0200

    init reming grain

--- Silo/Reminders/IRemindGrain.cs
+++ Silo/Reminders/IRemindGrain.cs
@@ -4,12 +4,17 @@ namespace Silo.Reminders;
 
 public interface IRemindGrain : IGrainWithStringKey
 {
-    
+    ValueTask Init();
 }
 
 public class RemindGrain : Grain, IRemindGrain, IRemindable
 {
     public Task ReceiveReminder(string reminderName, TickStatus status)
+    {
+        throw new NotImplementedException();
+    }
+
+    public ValueTask Init()
     {
         throw new NotImplementedException();
     }
